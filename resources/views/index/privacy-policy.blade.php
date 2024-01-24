9038b63410711127b20c99684e898ec2d5f0aaff
applaud-web-app <101401063+applaud-web-app@users.noreply.github.com>
2024-01-24 17:45:38 +0530

    Update privacy-policy.blade.php

--- resources/views/index/privacy-policy.blade.php
+++ resources/views/index/privacy-policy.blade.php
@@ -11,26 +11,97 @@
           
         </div>
 
-        <div class="p-6 bg-white dark:bg-slate-900 shadow dark:shadow-gray-700 rounded-md">
-            <h5 class="text-lg  mb-4">Overview :</h5>
-            <p class="text-slate-400">It seems that only fragments of the original text remain in the Lorem Ipsum texts used today. One may speculate that over the course of time certain letters were added or deleted at various positions within the text.</p>
-            <p class="text-slate-400">In the 1960s, the text suddenly became known beyond the professional circle of typesetters and layout designers when it was used for Letraset sheets (adhesive letters on transparent film, popular until the 1980s) Versions of the text were subsequently included in DTP programmes such as PageMaker etc.</p>
-            <p class="text-slate-400">There is now an abundance of readable dummy texts. These are usually used when a text is required purely to fill a space. These alternatives to the classic Lorem Ipsum texts are often amusing and tell short, funny or nonsensical stories.</p>
-        
-            <h5 class="text-lg  mb-4 mt-6">We use your information to :</h5>
-            <ul class="list-none text-slate-400 mt-4">
-                <li class="flex mt-2"><i class="uil uil-arrow-right text-indigo-600 text-lg align-middle me-2"></i>Digital Marketing Solutions for Tomorrow</li>
-                <li class="flex mt-2"><i class="uil uil-arrow-right text-indigo-600 text-lg align-middle me-2"></i>Our Talented &amp; Experienced Marketing Agency</li>
-                <li class="flex mt-2"><i class="uil uil-arrow-right text-indigo-600 text-lg align-middle me-2"></i>Create your own skin to match your brand</li>
-                <li class="flex mt-2"><i class="uil uil-arrow-right text-indigo-600 text-lg align-middle me-2"></i>Digital Marketing Solutions for Tomorrow</li>
-                <li class="flex mt-2"><i class="uil uil-arrow-right text-indigo-600 text-lg align-middle me-2"></i>Our Talented &amp; Experienced Marketing Agency</li>
-                <li class="flex mt-2"><i class="uil uil-arrow-right text-indigo-600 text-lg align-middle me-2"></i>Create your own skin to match your brand</li>
-            </ul>
+        <div class="p-6 bg-white dark:bg-slate-900 shadow dark:shadow-gray-700 rounded-md text-slate-500">
+            <h5 class="text-lg text-gray-900 mb-3">Overview :</h5>
+            <p class="mb-3">This policy sets out how The School of Dental Nursing (‘the School’) uses and protects any information that you give to the School. The School is committed to safeguarding the personal information you give to us and to protect your privacy. By submitting your personal information via our website: schoolofdentalnursing.com you are confirming that you have read and understood how we may use the personal information that you have provided .</p>
 
-            <h5 class="text-lg  mb-4 mt-6">Information Provided Voluntarily :</h5>
-            <p class="text-slate-400">In the 1960s, the text suddenly became known beyond the professional circle of typesetters and layout designers when it was used for Letraset sheets (adhesive letters on transparent film, popular until the 1980s) Versions of the text were subsequently included in DTP programmes such as PageMaker etc.</p>
+            <h5 class=" text-gray-900 mb-3">Data Controller Contact Details</h5>
+            <p class="mb-3">The Data Controller is The School of Dental Nursing, 3 The Mount, Acton, London, W3 9NW
+                0208993 4500, <b>dental[@]schoolofdentalnursing.com</b>
+                </p>
+          
+                <h5 class="text-lg text-gray-900 mb-3">1. Collecting personal information</h5>
+                    <p>We collect personal information about you when you register with us through our:</p>
 
-           
+                    <ul>
+                        <li class="list-disc ml-5"> Electronic registration/enquiry forms on schoolofdentalnursing.com.</li>
+                        <li class="list-disc ml-5">
+                             When you apply for one of our courses online.</li>
+                             <li class="list-disc ml-5">
+                           When you comment or write your testimonial (Student Success Stories) on your course. Only your name and course will be shown on the public-facing website. This information is only used to identify you as a contributor to the comment/testimonial section of the website post and is not passed on to any of the third-party data processors detailed below in section 5.0. Your comment and its associated personal data will remain on our site until we see fit to either remove the comment or remove the comment/testimonial post. Should you wish to have the comment/testimonial and its associated personal data deleted, please contact us.</li>
+                           <li class="list-disc ml-5">
+                            Student Information System The Student Information System is a dedicated software as service app which helps us manage all the applications within the recruitment process. All the data stored within the Student Information System is stored securely and managed only by the School within a dedicated hosting space.</li>
+                            <li class="list-disc ml-5">
+                           Internal Databases</li>
+                        
+                            
+                        
+                    </ul>
+                    <p class="mb-3">
+                        Personal data directly related to the education process and the progress of all individual students may be stored in the School’s internal databases. In this case all the data is stored on the internal server owned and managed by the School. Data is collected securely. Access rights administration ensures that the data is available only to limited and authorised members of staff.
+                    </p>
+
+                    <h5 class="text-lg text-gray-900 mb-3">2. Cookies and site browsing tracking</h5>
+
+                    <p class="mb-3">In addition to your personal information when you register, when you use our website, we also collect tracking information such as your browser type, the type of operating system you use, your IP address, and pages visited on the site. This information helps us increase the user-friendliness of our website and to better tailor our website to your needs. We use cookies and Google Analytics to track user interaction, to determine the number of people using our site, to better understand how people find and use our web pages and to see their journey through the website. We consider Google to be a third-party data processor (see section 6.0 below).</p>
+
+<h5 class="text-lg text-gray-900 mb-3">3. How the information is used</h5>
+<p class="mb-3">We collect the personal information supplied by you as set out in section 1.0 so that we can send you requested relevant information on courses or information that will be of interest to you. You will be updated regularly throughout the year with our newsletters and e-shots – and each time we communicate with you there will be the opportunity to unsubscribe. We collect information for the following purposes: – To reply to your enquiries more efficiently about the services we provide. – To send you news items, including information on courses you might be interested in by email. – To inform you of new courses, products and services that may be of interest to you by email. – To conduct market research by email.</p>
+
+<h5 class="text-lg text-gray-900 mb-3">4. Marketing Automation</h5>
+<p class="mb-3">The School uses marketing automation which means that some information is provided to our students by automated technology without human intervention. The information is provided always as a result of customer’s request made by the contact form and giving them the right to unsubscribe and stop communication. ABI College performs regular checks if automated service works in order to fulfil its objectives.</p>
+
+
+<h5 class="text-lg text-gray-900 mb-3">5. Sharing Information with external bodies and organisations</h5>
+<p>We may share specific parts of the student information which we hold with our external partners such as awarding or regulatory bodies. The reasons we do this are as follows: - To manage our relationship with external organisations/bodies in order to meet our regulatory obligations – To allow a partner awarding organisation/body to check if a student application meets eligibility criteria and indicates suitability for the course applied for. The School collects special categories of personal data such as nationality, ethnic origin, religious belief, sexual orientation, disabilities, and criminal convictions. This is done during the recruitment process and applicants are asked to give their consent at this stage. This category of data is collected to meet the statistical requirements of external organisations and stakeholders and the original consent implies acceptance of such disclosure.</p>
+
+
+<h5 class="text-lg text-gray-900 mb-3">6. Third party data processors</h5>
+<p>We use a number of third parties to collect and process personal data on our behalf. These third parties have been carefully chosen and all of them comply with data regulations <br>
+The third party data processors we use are: <br>
+•	Google (Privacy Policy) <br>
+•	Mailchimp (Privacy Policy) <br>
+•	Hubspot (Privacy Policy) <br>
+•	Krystal (Privacy Policy) (where our site is hosted and data stored) </p>
+
+
+<h5 class="text-lg text-gray-900 mb-3">7. Strict no sharing policy</h5>
+<p class="mb-3">We will never share your email address details with third parties (excluding our third-party data processors detailed in section 6.0). We NEVER sell our data or give access to it to anyone else so no third parties will contact you. All direct communication comes from the School. From time to time we may share aggregated anonymous information about visitors to our site to business partners such as the number of users we have, their average age, location, etc. This information will be shared in a way that ensures your identity is not revealed. We may also disclose or access your account if required to do so by law including governmental agencies.</p>
+
+
+<h5 class="text-lg text-gray-900 mb-3">8. Lawful basis for collecting data</h5>
+<p class="mb-3">The School constitutes a lawful basis for processing personal data and complies with Article 6 of the GDPR categories: (a) Consent: and (f) Legitimate interests.</p>
+
+
+<h5 class="text-lg text-gray-900 mb-3">9. Security of personal information against cyber-attacks & breaches</h5>
+<p class="mb-3">Our technical services provider holds all student data on their servers to process this information, however they do not use it. Although we use HTTPS connections to encrypt and protect your information, sending data over the internet is not completely secure, so we cannot guarantee the security of your personal information while it’s in transit. Once we receive your personal information our technical services provider has procedures and security features in place to ensure that it is protected against cyber-attacks and potential breaches.</p>
+
+<h5 class="text-lg text-gray-900 mb-3">10. Links from our websites</h5>
+<p class="mb-3">Our websites contain links to other websites, which are not controlled by the School This Privacy Statement applies only to our sites. Please note if you click on a external link, the click may take you off our website.</p>
+
+<h5 class="text-lg text-gray-900 mb-3">11. Unsubscribing & removing you School data</h5>
+<p class="mb-3">You can unsubscribe from our email services by clicking the ‘unsubscribe’ link in any of our email communication to you. This will not remove your personal data associated with your courses or studies. If you have previously submitted a student application, newsletter subscription or participated in our surveys, we may still hold some of the information you provided even after you unsubscribe from our emails. If you would like us to fully remove your information please contact us with your request at mydata[@]schoolofdentalnursing.com including your full name and email address. We will ensure that it is removed in line with GDPR policies and timescales on request.</p>
+
+
+<h5 class="text-lg text-gray-900 mb-3">12. Data Retention</h5>
+<p class="mb-3">The School retains personal data no longer than is necessary for the purpose they were collected.</p>
+
+
+<h5 class="text-lg text-gray-900 mb-3">13. Your rights</h5>
+<p class="mb-3">If you need more information about your rights (such as to be forgotten (?), to access information, to be informed receive information?, to withdrawn consent) in relation to your personal data and ABI College or you would like to execute your rights, please contact us at mydata[@]schoolofdentalnursing.com</p>
+
+<h5 class="text-lg text-gray-900 mb-3">14. Complaints</h5>
+<p class="mb-3">If you are unhappy with how we deal with your personal data, you can make a complaint utilising our complaints procedure in the first instance (LiNK). If you want to make complaint, first, please consider contacting us is informal way and we will do everything to put the things right. You can contact us mydata[@]schoolofdentalnursing.com</p>
+
+<h5 class="text-lg text-gray-900 mb-3">11. Privacy policy changes</h5>
+<p class="mb-3">If our privacy policy changes significantly, we will contact you by email and advise you what has changed. This email will contain an unsubscribe link like all our other email correspondence giving you the option to opt out.
+</p>
+
+
+
+
+            
+
         </div>
 
     </div>
